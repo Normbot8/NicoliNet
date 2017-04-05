2ec2bdd97cbe4c2f48933e2c03fdef8e0a1e12ed
Normbot8 <normbot8@gmail.com>
2017-04-05 18:17:06 -0400

    Add the IP location freak looty loot loot
    Bwahahahahaha! NicoliNet knows where you are!

---
+++ javascript/location.js
@@ -0,0 +1,4 @@
+$.get("http://freegeoip.net/json/", function (response) {
+    $("#address").html("Thank you for visiting from " + response.city + ", " + response.region_name + "!");
+    $('#latlong').html("For your information (and also to freak you out further): Yer Latitude: " + response.latitude + " Yer Longitude: " + response.longitude);
+}, "jsonp");
