1eb3c5862d143d8128941e7adb0f2f39690c3193
Normbot8 <daveterrimartin@TerriMatinsiMac.home>
2017-05-05 18:36:06 -0400

    JS Update
    Cool customization features! (click on the end to find them)

---
+++ javascript/customizer.js
@@ -0,0 +1,38 @@
+$(function () {
+    if (localStorage.getItem('hasAccount') === 'true') {
+        var firstName = localStorage.getItem('firstName');
+        var middleName = localStorage.getItem('middleName');
+        var lastName = localStorage.getItem('lastName');
+        var nickName = localStorage.getItem('nickName');
+        var backColor = localStorage.getItem('backgroundColor');
+        var betaTester = localStorage.getItem('betaTester');
+        var astley = localStorage.getItem('astleyify');
+        $('body').css('background-color', backColor);
+        if (astley === "yes") {
+            $('body').append('<audio id=\"astleySong\" src=\"sound/astley.ogg\" loop></audio>');
+            var astleySong = document.getElementById('astleySong');
+            astleySong.play();
+        }
+        if (nickName === "") {
+            $('.heading h2').text(firstName + ' ' + middleName + ' ' + lastName + "\'s NicoliNet");
+        } else if (nickName != "") {
+            $('.heading h2').text(nickName + '\'s NicoliNet');
+        }
+        var page = window.location.pathname;
+        if (page === '/index.html') {
+            if (nickName === "") {
+                $('#mydudes').text(firstName + ' ' + middleName + ' ' + lastName);
+            } else if (nickName != "") {
+                $('#mydudes').text(nickName);
+            }
+        }
+        if (page === '/1h@nky0u.html') {
+            if (nickName === "") {
+                $('#recipent').val(firstName + ' ' + middleName + ' ' + lastName);
+            } else if (nickName != "") {
+                $('#recipent').val(nickName);
+            }
+        }
+
+    }
+})
--- javascript/print.js
+++ javascript/print.js
@@ -1,9 +1,18 @@
 function printElement() {
-    var cardWidth = document.getElementById('cardwidth').value;
-    var cardHeight = document.getElementById('cardheight').value;
+    var cardWidth = document.getElementById('cardwidth').value + 'in';
+    var cardHeight = document.getElementById('cardheight').value + 'in';
     var fontSize = document.getElementById('fontsize').value;
-    var style = 'width: ' + cardWidth + 'in; height: ' + cardHeight + 'in; font-size: ' + fontSize + 'px; border-width: 0px; resize: none;'
-    var textArea = '<textarea style=\'' + style + '\'>'
+    if (cardHeight === 'in') {
+        cardHeight = '150px';
+    }
+    if (cardWidth === 'in') {
+        cardWidth = '300px';
+    }
+    if (fontSize === '') {
+        fontSize = '13';
+    }
+    var style = 'width: ' + cardWidth + '; height: ' + cardHeight + '; font-size: ' + fontSize + 'px; border-width: 0px; resize: none;';
+    var textArea = '<textarea style=\'' + style + '\'>';
     var pri = document.getElementById("ifmcontentstoprint").contentWindow;
     var message = textArea + msg + '</textarea>';
     pri.document.open();
