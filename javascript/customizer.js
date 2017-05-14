1a46e8969f2ae1b10947feaa5198469ae2a2b0f9
Normbot8 <normbot8@gmail.com>
2017-05-14 18:05:54 -0400

    Hopefully finished update

--- javascript/customizer.js
+++ javascript/customizer.js
@@ -18,7 +18,7 @@ $(function () {
             $('.into-pic').replaceWith('<div class=\"into-pic\"><img src=\"pics/nicoliwham.png\" alt=\"The Grate Nicoli\"></div>');
             $('body').append('<iframe width=\"100\" height=\"100\" src=\"https://www.youtube.com/embed/gpqmoBYkQfc\?autoplay=1&loop=1&playlist=gpqmoBYkQfc\" frameborder=\"0\" style=\'z-index:-10000; position:fixed\'></iframe>');
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                $('.into-pic').replaceWith('<div class=\"into-pic\"><iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/gpqmoBYkQfc\?autoplay=1&loop=1&playlist=gpqmoBYkQfc\" frameborder=\"0\"></iframe></div>');
+                $('.into-pic').replaceWith('<div class=\"into-pic\"><iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/gpqmoBYkQfc\?autoplay=1&loop=1&playlist=gpqmoBYkQfc\" frameborder=\"0\" style=\'display:block; margin: auto;\'></iframe></div>');
             }
         }
         if (nickName === "") {
