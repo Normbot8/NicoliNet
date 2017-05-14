bade8509b33dff1dfbc2dcbca3649a3b9ff95d17
Normbot8 <normbot8@gmail.com>
2017-05-14 18:02:49 -0400

    I hope this works

--- javascript/customizer.js
+++ javascript/customizer.js
@@ -12,7 +12,7 @@ $(function () {
             $('.into-pic').replaceWith('<div class=\"into-pic\"><img src=\"pics/nicoliastley.png\" alt=\"The Grate Nicoli\"></div>');
             $('body').append('<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/k5pL79ywoTY\?autoplay=1&loop=1&playlist=k5pL79ywoTY\" frameborder=\"0\" allowfullscreen style=\'z-index:-10000; position:fixed\'></iframe>');
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                $('.into-pic').replaceWith('<div class=\"into-pic\"><iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/k5pL79ywoTY\?autoplay=1&loop=1&playlist=k5pL79ywoTY\" frameborder=\"0\" allowfullscreen></iframe></div>');
+                $('.into-pic').replaceWith('<div class=\"into-pic\"><iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/k5pL79ywoTY\?autoplay=1&loop=1&playlist=k5pL79ywoTY\" frameborder=\"0\" allowfullscreen style=\'display:block; margin: auto;\'></iframe></div>');
             }
         } else if (music === 'wham') {
             $('.into-pic').replaceWith('<div class=\"into-pic\"><img src=\"pics/nicoliwham.png\" alt=\"The Grate Nicoli\"></div>');
