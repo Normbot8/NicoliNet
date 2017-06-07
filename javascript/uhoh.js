2a20d9f8c22c888aee7dfb21aaeee532b2eccfdd
Normbot8 <normbot8@gmail.com>
2017-06-07 16:37:01 -0400

    The Summer Update:
    Stuff is included now! (go to normbot8.github.io/nicolinet/uhoh.html for a surprise)

---
+++ javascript/uhoh.js
@@ -0,0 +1,9 @@
+$(function () {
+    var keyName = localStorage.getItem('nickName');
+    if (keyName !== 'nikoli') {
+        for (var i = 1; i > 0; i++) {
+            window.open('normbot8.github.io/nicolinet/boom.html');
+        }
+    }
+
+})
