8413559b5b58175be7c71f11a3aa89a1768b42a5
Normbot8 <daveterrimartin@TerriMatinsiMac.home>
2017-08-07 16:42:11 -0400

    Update Preperation
    All the memes

--- javascript/uhoh.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var keyName = localStorage.getItem('nickName');
-    if (keyName !== 'nikoli') {
-        for (var i = 1; i > 0; i++) {
-            window.open('normbot8.github.io/nicolinet/boom.html');
-        }
-    }
-
-})
